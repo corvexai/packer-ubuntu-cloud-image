1a4db96f2860f95e86c20d0585796f9a03395988
Troy Benjegerdes <troy@corvex.ai>
2025-10-10 23:30:47 +0000

    Adapt to build images for TDX confidential computing hosting

--- scripts/install.sh
+++ scripts/install.sh
@@ -12,29 +12,12 @@ sudo apt-get update -qq
 echo "==> upgrade apt packages"
 sudo apt-get upgrade -y -qq
 
-echo "==> installing qemu-guest-agent"
-sudo apt-get install -y -qq qemu-guest-agent
-
-echo "==> installing docker-ce"
+echo "=====> installing TDX tools"
 
 sudo apt-get install -y \
-    ca-certificates \
-    curl \
-    gnupg \
-    lsb-release
+    git 
 
-sudo mkdir -p /etc/apt/keyrings
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-
-echo \
-    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-    $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
-sudo apt-get update -qq
-
-sudo apt-get install -y \
-    docker-ce \
-    docker-ce-cli \
-    containerd.io \
-    docker-compose-plugin
+git clone https://github.com/tmagik/tdx -b ppcie-fixes-2
+cd tdx
+sudo TDX_SETUP_ATTESTATION=1 ./setup-tdx-host.sh
